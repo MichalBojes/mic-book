e98aa26d9aac20bf0e4c57904f91be5c9c4ba67e
Michał Bojęś <mbojes1@gmail.com>
2019-11-05 02:10:23 +0100

    initialCommit

---
+++ mic-book-server/src/main/java/com/micbook/app/micbook/MicBookApplication.java
@@ -0,0 +1,13 @@
+package com.micbook.app.micbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicBookApplication.class, args);
+	}
+
+}
---
+++ mic-book-server/src/test/java/com/micbook/app/micbook/MicBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micbook.app.micbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
